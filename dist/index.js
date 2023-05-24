95b853375bc310f5e1b561c1d9cb6b7a1a2bed4b
yhyem <yoonhyemin0120@gmail.com>
2023-05-24 22:57:29 +0900

    refactor: 변수명 상세히 변경

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const input_1 = require("./input");
-const Main = async () => {
+const main = async () => {
     console.log("학생 관리 프로그램에 오신 여러분들 환영합니다");
     console.log(`
   1. Create a new student
@@ -34,17 +34,17 @@ const Main = async () => {
             }
             case "3": {
                 const selectId = await (0, input_1.input)("Select Find Id: ");
-                const findArray = student.filter((data) => data.id == Number(selectId));
-                console.log(findArray);
+                const findStudent = student.find((data) => data.id == Number(selectId));
+                console.log(findStudent);
                 option = await (0, input_1.input)("Select an option: ");
                 break;
             }
             case "4": {
-                const newId = await (0, input_1.input)("아이디를 입력해주세요: ");
+                const FindId = await (0, input_1.input)("아이디를 입력해주세요: ");
                 const newName = await (0, input_1.input)("이름을 입력해주세요: ");
                 const newAge = await (0, input_1.input)("나이를 입력해주세요: ");
                 student.find((data) => {
-                    if (data.id === Number(newId)) {
+                    if (data.id === Number(FindId)) {
                         data.name = newName;
                         data.age = +newAge;
                     }
@@ -55,8 +55,8 @@ const Main = async () => {
             }
             case "5": {
                 const selectId = await (0, input_1.input)("Select Deleted Id: ");
-                const changeArray = student.filter((data) => data.id !== Number(selectId));
-                student = changeArray;
+                const newArray = student.filter((data) => data.id !== Number(selectId));
+                student = newArray;
                 console.log(student);
                 option = await (0, input_1.input)("Select an option: ");
                 break;
@@ -64,4 +64,4 @@ const Main = async () => {
         }
     }
 };
-Main();
+main();
