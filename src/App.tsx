c657a2257f1c658d748f82e594a92e4655945fd7
yhyem <yoonhyemin0120@gmail.com>
2023-12-28 20:57:32 +0900

    feat: 카카오 로그인 테스트 파일

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import { TimeTable } from "./timeTable/templates";
 import { RecoilEnv } from "recoil";
+import KakaoLogin from "./kakaoLogin/pages";
 
 RecoilEnv.RECOIL_DUPLICATE_ATOM_KEY_CHECKING_ENABLED = false;
 
 function App() {
   return (
     <>
-      <TimeTable />
+      <KakaoLogin />
     </>
   );
 }
---
+++ src/kakaoLogin/pages.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const kakaoLogin = () => {
+  return <>카카오 로그인 테스트</>;
+};
+
+export default kakaoLogin;
