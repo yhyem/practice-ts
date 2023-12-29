20de0639a50a567729113cb8aae9627d59b44c96
yhyem <yoonhyemin0120@gmail.com>
2023-12-29 14:06:17 +0900

    feat: 카카오 소셜 로그인 테스트 구현

--- src/kakaoLogin/pages.tsx
+++ src/kakaoLogin/pages.tsx
@@ -1,7 +1,19 @@
-import React from "react";
+const KakaoLogin = () => {
+  const Rest_api_key = process.env.REACT_APP_KAKAO_API_KEY; //REST API KEY
+  const redirect_uri = "http://localhost:3000/auth"; //Redirect URI
 
-const kakaoLogin = () => {
-  return <>카카오 로그인 테스트</>;
+  // oauth 요청 URL
+  const kakaoURL = `https://kauth.kakao.com/oauth/authorize?client_id=${Rest_api_key}&redirect_uri=${redirect_uri}&response_type=code`;
+
+  const handleLogin = () => {
+    window.location.href = kakaoURL;
+  };
+
+  return (
+    <>
+      <button onClick={handleLogin}>카카오 로그인</button>
+    </>
+  );
 };
 
-export default kakaoLogin;
+export default KakaoLogin;
