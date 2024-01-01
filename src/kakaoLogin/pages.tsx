09555f01cb1566094eb01f68ffc76c3876d18df6
yhyem <yoonhyemin0120@gmail.com>
2024-01-01 20:55:13 +0900

    chore: redirect_uri env 분리

--- src/kakaoLogin/pages.tsx
+++ src/kakaoLogin/pages.tsx
@@ -1,6 +1,6 @@
 const KakaoLogin = () => {
   const Rest_api_key = process.env.REACT_APP_KAKAO_API_KEY; //REST API KEY
-  const redirect_uri = "http://localhost:3000/auth"; //Redirect URI
+  const redirect_uri = process.env.REACT_APP_REDICRECT_URI;
 
   // oauth 요청 URL
   const kakaoURL = `https://kauth.kakao.com/oauth/authorize?client_id=${Rest_api_key}&redirect_uri=${redirect_uri}&response_type=code`;
