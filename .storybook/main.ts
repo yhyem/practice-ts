beb181165f780b5a80bf7bd41cd17ab661406006
yhyem <yoonhyemin0120@gmail.com>
2024-01-20 22:08:16 +0900

    feat: storybook styled-component svg 허용

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,24 +1,40 @@
-module.exports = {
-  stories: ["../src/components/**/*.stories.js"],
-  staticDirs: ["../public"],
+const TsconfigPathsPlugin = require("tsconfig-paths-webpack-plugin");
 
+const config: StorybookConfig = {
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/preset-create-react-app",
+    "@storybook/addon-onboarding",
     "@storybook/addon-interactions",
   ],
-
-  features: {
-    postcss: false,
-  },
-
   framework: {
-    name: "@storybook/react-webpack5",
-    options: {}
+    name: "@storybook/nextjs",
+    options: {},
   },
-
   docs: {
-    autodocs: true
-  }
+    autodocs: "tag",
+  },
+  webpackFinal: async (config) => {
+    const imageRule = config.module?.rules?.find((rule) => {
+      const test = (rule as { test: RegExp }).test;
+
+      if (!test) {
+        return false;
+      }
+
+      return test.test(".svg");
+    }) as { [key: string]: any };
+
+    imageRule.exclude = /\.svg$/;
+
+    config.module?.rules?.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
+
+    return config;
+  },
 };
+
+export default config;
